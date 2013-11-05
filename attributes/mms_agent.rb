4e6e27404b8b62adf02d97e1589bb5dbbf1abd9c
Matt Chu <mchu@kabam.com>
2013-11-05 13:00:59 -0800

    [#56553362] there's already an update to mms-agent

--- attributes/mms_agent.rb
+++ attributes/mms_agent.rb
@@ -5,6 +5,6 @@ default['mongodb']['mms_agent']['install_dir'] = '/usr/local/share'
 default['mongodb']['mms_agent']['log_dir'] = "#{node[:mongodb][:logpath]}/agent"
 
 # because the agent download url is currently not versioned, this solely documentation with no actual checks in place
-default['mongodb']['mms_agent']['version'] = '1.6.1'
+default['mongodb']['mms_agent']['version'] = '1.6.2'
 # this is only an optimization, new versions will still be downloaded; see http://docs.opscode.com/resource_remote_file.html for details about the checksum attribute
-default['mongodb']['mms_agent']['checksum'] = '498181cbaf9b24f9dfbf70a5bacb0d238d368793d2c1a270cf636793c4eb3a55'
+default['mongodb']['mms_agent']['checksum'] = '0bdc41d8ec48db83c879d8cfdf0f370055d38601d98c0d17cc4416f4cebe2d80'
