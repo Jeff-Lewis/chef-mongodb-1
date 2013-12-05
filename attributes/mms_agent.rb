ee6f3781ebc50cb93eab5ea5f032e55465fbb726
Matt Chu <mchu@kabam.com>
2013-12-05 14:38:48 -0800

    [#61007096] code review: better docs about mms-agent dir

--- attributes/mms_agent.rb
+++ attributes/mms_agent.rb
@@ -3,6 +3,8 @@ default[:mongodb][:mms_agent][:secret_key] = ""
 
 # shouldn't need to changed, but configurable anyways
 default[:mongodb][:mms_agent][:install_url] = "https://mms.mongodb.com/settings/mms-monitoring-agent.zip"
+# N.B. the dir MUST be named mms-agent; this is the contents of the unarchived zip
+# the location of the dir (i.e. /usr/local/share) can be freely changed
 default[:mongodb][:mms_agent][:install_dir] = "/usr/local/share/mms-agent"
 default[:mongodb][:mms_agent][:log_dir] = "#{node[:mongodb][:logpath]}/agent"
 default[:mongodb][:mms_agent][:install_munin] = true
