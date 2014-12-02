51491b383e9e3c6b2abcd2dfaa969db83af60083
Michael Reeves <mreeves@kabam.com>
2014-12-02 14:47:33 -0800

    [#83716736] Update mms backup & monitoring agents
    - Change package install procedure
    - Add more tests

--- spec/mms_monitoring_agent_spec.rb
+++ spec/mms_monitoring_agent_spec.rb
@@ -8,10 +8,14 @@ describe 'mongodb::mms-agent' do
     end
   end
 
+  # This is designed so if folks change the version in the attributes file this will fail 
   it 'package install the mms_monitoring_agent' do
     chef_run.converge(described_recipe)
     expect(chef_run).to include_recipe('mongodb::mms-agent')
-    expect(chef_run).to install_package('mongodb-mms-monitoring-agent')
+    expect(chef_run).to install_package('mongodb-mms-monitoring-agent').with_version('2.8.0.143-1')
+    expect(chef_run).to create_remote_file("#{Chef::Config[:file_cache_path]}/mms_agent.deb").with( 
+      :source => 'https://mms.mongodb.com/download/agent/monitoring/mongodb-mms-monitoring-agent_2.8.0.143-1_amd64.deb'
+    )
     expect(chef_run).to render_file('/etc/mongodb-mms/monitoring-agent.config').with_content(/.*=dummy_key/)
     resource = chef_run.template('/etc/mongodb-mms/monitoring-agent.config')
     expect(resource).to notify('service[mongodb-mms-monitoring-agent]').to(:restart).delayed
